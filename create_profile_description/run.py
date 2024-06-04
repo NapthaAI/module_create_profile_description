de70c55643c57c4664aaaeb5a627e4cb547c5765
Richard Blythman <richardblythman@gmail.com>
2024-06-04 13:52:29 +0100

    new input parameters

--- create_profile_description/run.py
+++ create_profile_description/run.py
@@ -1,15 +1,15 @@
 from dotenv import load_dotenv
 from litellm import completion
 import os
-import yaml
 from create_profile_description.schemas import InputSchema
-from create_profile_description.utils import get_logger
+from naptha_sdk.utils import get_logger, load_yaml
+from typing import Dict
 
 load_dotenv()
 logger = get_logger(__name__)
 
-def run(inputs: InputSchema, cfg: dict = None) -> str:
-    logger.info(f'Running job with inputs: {inputs}')
+def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None, flow_run = None, cfg: Dict = None) -> str:
+    logger.info(f'Running module with inputs: {inputs}')
     
     messages = [
         {"role": "system", "content": cfg["inputs"]["system_message"]},
@@ -38,11 +38,10 @@ def run(inputs: InputSchema, cfg: dict = None) -> str:
 
 if __name__ == "__main__":
     cfg_path = f"create_profile_description/component.yaml"
-    with open(cfg_path, "r") as file:
-        cfg = yaml.load(file, Loader=yaml.FullLoader)
+    cfg = load_yaml(cfg_path)
 
     input_data = {
         "points": ["Loves to travel", "Enjoys reading", "Loves to cook"]
     }
     inputs = InputSchema(**input_data)
-    print(run(inputs, cfg))
+    print(run(inputs, cfg=cfg))
